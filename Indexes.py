dc4cdf53f45eb5dc69f7346be202f47497ff59a8
NikitaKorablev <kit06092003@gmail.com>
2020-06-23 22:35:44 +0300

    23.06.2020

--- Indexes.py
+++ Indexes.py
@@ -27,27 +27,23 @@ def obrez (hist):
     procent = int(sum*0.05)
     print (procent, ' ', len(hist))
     plus = 0
-    
     for i in range (0, len(hist)):
         plus += hist[i]
         if plus >= procent:
-            misha = i
-            print (misha)
+            misha = i-1
+            print (misha, ' ', hist[misha])
             break
     
     plus = 0
-    
     hist = hist[::-1]
-    
     for i in range (misha, len(hist)):
         plus += hist[i]
         if plus >= procent:
-            maximum = len(hist) - i + 1
-            print (maximum)
+            maximum = len(hist) - i
+            print (maximum, ' ', hist[maximum])
             break
 
     hist = hist[::-1]  
-    
     plt.plot(hist)
     plt.show()
     
@@ -58,12 +54,11 @@ def obrez (hist):
 
     plt.plot(hist_vos)
     plt.show()
-    
     return hist
 
 
     
-folder = r'D:\for_python_scripts\Nizni_chanels\Landsat_B'
+folder = r'D:\NOU2020\EarthExplorer\nnovgorod\2018\23-JUN\REFLECTANCE\Landsat_B'
 b1 = np.load(folder + '1.npy')
 b2 = np.load(folder + '2.npy')
 #b3 = np.load(folder + '3.npy')
@@ -95,7 +90,7 @@ print (np.max(MNDWI),' ',np.min(MNDWI))
 plt.title("MNDWI")
 h1 = histogram(MNDWI, 100, np.min(MNDWI), np.max(MNDWI))
 h1_v2 = obrez(h1)
-print(h1_v2)
+
 
 
 """ NDWI = (Blue − Red)/(Blue + Red) """
@@ -106,7 +101,7 @@ NDWI = np.divide(a,b)
 print (np.max(NDWI),' ',np.min(NDWI))
 plt.title("NDWI")
 h2 = histogram(NDWI, 100, np.min(NDWI), np.max(NDWI))
-
+h2_v2 = obrez(h2)
 
 
 """ AWEInsh = 4*(Blue - Near) - (0.25*Red + 2.75*SWIR2) """
@@ -114,7 +109,7 @@ AWEInsh = 4*(b2 - b5) - (0.25*b4 + 2.75*b7)
 print (np.max(AWEInsh),' ',np.min(AWEInsh))
 plt.title("AWEInsh")
 h3 = histogram(AWEInsh, 100, np.min(AWEInsh), np.max(AWEInsh))
-
+h3_v2 = obrez(h3)
 
 
 """ AWEIsh = CA + 2.5*Blue - 1.5*(Red + Near) - 0.25*SWIR2 """
@@ -122,7 +117,7 @@ AWEIsh = b1 + 2.5*b2 - 1.5*(b4 + b5) - 0.25*b7
 print (np.max(AWEIsh),' ',np.min(AWEIsh))
 plt.title("AWEIsh")
 h4 = histogram(AWEIsh, 100, np.min(AWEIsh), np.max(AWEIsh))
-
+h4_v2 = obrez(h4)
 
 
 
