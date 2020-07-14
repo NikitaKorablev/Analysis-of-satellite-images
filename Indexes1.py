98f674e3c0918360aced37a540f92a45466dbb45
NikitaKorablev <kit06092003@gmail.com>
2020-07-14 20:39:52 +0300

    14.07.2020 20:39

--- Indexes1.py
+++ Indexes1.py
@@ -11,46 +11,62 @@ import matplotlib.pyplot as plt
 def histogram (array, bins, hmin, hmax):
     hist = np.zeros(bins)
     step = (hmax - hmin)/(bins-1)
+    
     for i in range (0, array.shape[0]):
         for m in range (0, array.shape[1]):
-            indx = int((array[i,m] - hmin)//step)
-            hist[indx] = hist[indx]+1
+            if array[i,m] <= hmax and array[i,m] >= hmin:            
+                indx = int((array[i,m] - hmin)//step)
+                hist[indx] = hist[indx]+1
                   
     plt.plot(hist)
     plt.show()
     return hist
 
+
+def hist_min_max (array, bins, imin, imax, hmin, hmax):
+    hist = np.zeros(bins)
+    step = (hmax - hmin)/(bins-1)
+    hmin += step*imin
+    hmax2 = hmax
+    hmax2 += step*imax
+    hmax = hmax2 - hmax
+    print (step, ' ', hmin, ' ', hmax)
+    for i in range (0, array.shape[0]):
+        for m in range (0, array.shape[1]):
+            if array[i,m] <= hmax and array[i,m] >= hmin:            
+                indx = int((array[i,m] - hmin)//step)
+                hist[indx] = hist[indx]+1
+                  
+    plt.plot(hist)
+    plt.show()
+    return hist
+
+
 def obrez (hist):  
-    mi_ma = []
 #    sum_per = 0 
 #    hist_vos = []
     sum = np.sum(hist)
-    procent = int(sum*0.03)
+    procent = int(sum*0.1)
     print (procent, ' ', len(hist))
     plus = 0
     for i in range (0, len(hist)):
         plus += hist[i]
         if plus >= procent:
-            minimum = i
-            print (minimum, ' ', hist[minimum])
-            mi_ma.append(hist[minimum])
+            minimum = i - 1
+            print (minimum)
             break
         
     plus = 0
     hist = hist[::-1]
-    for i in range (minimum, len(hist)):
+    for i in range (0, len(hist)):
         plus += hist[i]
         if plus >= procent:
             maximum = len(hist) - i
-            print (maximum, ' ', hist[maximum])
-            mi_ma.append(hist[maximum])            
+            print (maximum)          
             break
     
     hist = hist[::-1]  
 
-    plt.plot(hist)
-    plt.show()
-    
 #    for n in hist:
 #        sum_per += n
 #        sum_per = float(sum_per)
@@ -58,7 +74,7 @@ def obrez (hist):
 #
 #    plt.plot(hist_vos)
 #    plt.show()
-    return mi_ma
+    return minimum, maximum
 
     
 folder = r'D:\NOU2020\EarthExplorer\nnovgorod\2018\23-JUN\REFLECTANCE\Landsat_B'
@@ -89,46 +105,45 @@ a = b2 - b5
 b = b2 + b5
 b[b == 0] = 1
 MNDWI = np.divide(a,b)
-print (np.max(MNDWI),' ',np.min(MNDWI))
+print (np.min(MNDWI),' ',np.max(MNDWI))
 plt.title("MNDWI")
 h1 = histogram(MNDWI, 100, np.min(MNDWI), np.max(MNDWI))
+
 mi_ma = obrez(h1)
-plt.title("MNDWI")
-h1 = histogram(MNDWI, 100, mi_ma[0], mi_ma[1])
+h1_v2 = hist_min_max(MNDWI, 100, mi_ma[0], mi_ma[1], np.min(MNDWI), np.max(MNDWI))
 
 
 
-""" NDWI = (Blue − Red)/(Blue + Red) """
-a = b2 - b4
-b = b2 + b4
-b[b == 0] = 1
-NDWI = np.divide(a,b)
-print (np.max(NDWI),' ',np.min(NDWI))
-plt.title("NDWI")
-h2 = histogram(NDWI, 100, np.min(NDWI), np.max(NDWI))
-mi_ma = obrez(h2)
+# """ NDWI = (Blue − Red)/(Blue + Red) """
+# a = b2 - b4
+# b = b2 + b4
+# b[b == 0] = 1
+# NDWI = np.divide(a,b)
+# print (np.max(NDWI),' ',np.min(NDWI))
+# plt.title("NDWI")
+# h2 = histogram(NDWI, 100, np.min(NDWI), np.max(NDWI))
+# mi_ma = obrez(h2)
 
 
 
-""" AWEInsh = 4*(Blue - Near) - (0.25*Red + 2.75*SWIR2) """
-AWEInsh = 4*(b2 - b5) - (0.25*b4 + 2.75*b7)
-print (np.max(AWEInsh),' ',np.min(AWEInsh))
-plt.title("AWEInsh")
-h3 = histogram(AWEInsh, 100, np.min(AWEInsh), np.max(AWEInsh))
-mi_ma = obrez(h3)
+# """ AWEInsh = 4*(Blue - Near) - (0.25*Red + 2.75*SWIR2) """
+# AWEInsh = 4*(b2 - b5) - (0.25*b4 + 2.75*b7)
+# print (np.max(AWEInsh),' ',np.min(AWEInsh))
+# plt.title("AWEInsh")
+# h3 = histogram(AWEInsh, 100, np.min(AWEInsh), np.max(AWEInsh))
+# mi_ma = obrez(h3)
 
 
 
-""" AWEIsh = CA + 2.5*Blue - 1.5*(Red + Near) - 0.25*SWIR2 """
-AWEIsh = b1 + 2.5*b2 - 1.5*(b4 + b5) - 0.25*b7
-print (np.max(AWEIsh),' ',np.min(AWEIsh))
-plt.title("AWEIsh")
-h4 = histogram(AWEIsh, 100, np.min(AWEIsh), np.max(AWEIsh))
-mi_ma = obrez(h4)
+# """ AWEIsh = CA + 2.5*Blue - 1.5*(Red + Near) - 0.25*SWIR2 """
+# AWEIsh = b1 + 2.5*b2 - 1.5*(b4 + b5) - 0.25*b7
+# print (np.max(AWEIsh),' ',np.min(AWEIsh))
+# plt.title("AWEIsh")
+# h4 = histogram(AWEIsh, 100, np.min(AWEIsh), np.max(AWEIsh))
+# mi_ma = obrez(h4)
 
 
 
 
 #AWEI = 4*(b3-b5)-(0.25*b5+2.75*b9)
 #print (np.max(AWEI),' ',np.min(AWEI))
-#проверка
