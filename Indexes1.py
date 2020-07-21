4039c1dd263592df6cf55173aadefe6f02e1f6da
Belokamenskaya <alexandra.belokamenskaya@gmail.com>
2020-07-21 20:02:37 +0300

    From for to numpy

--- Indexes1.py
+++ Indexes1.py
@@ -6,140 +6,125 @@ Created on Sun May 31 18:24:37 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
+from calc_indexes import *
+import gc
+import numpy as np
 
 
-def histogram (array, bins, hmin, hmax):
-    hist = np.zeros(bins)
-    step = (hmax - hmin)/(bins-1)
-    
-    for i in range (0, array.shape[0]):
-        for m in range (0, array.shape[1]):
-            if array[i,m] <= hmax and array[i,m] >= hmin:            
-                indx = int((array[i,m] - hmin)//step)
-                hist[indx] = hist[indx]+1
-                  
-    plt.plot(hist)
-    plt.show()
-    return hist
-
-
-def hist_min_max (array, bins, imin, imax, hmin, hmax):
-    hist = np.zeros(bins)
-    step = (hmax - hmin)/(bins-1)
-    hmin += step*imin
-    hmax -= step*(len(hist)-imax)
-    print (step, ' ', hmin, ' ', hmax)
-    for i in range (0, array.shape[0]):
-        for m in range (0, array.shape[1]):
-            if array[i,m] <= hmax and array[i,m] >= hmin:            
-                indx = int((array[i,m] - hmin)//step)
-                hist[indx] = hist[indx]+1
-                  
-    plt.plot(hist)
-    plt.show()
-    return hist
-
-
-def obrez (hist):  
+def obrez (hist, minHist, maxHist, percent):  
 #    sum_per = 0 
 #    hist_vos = []
-    sum = np.sum(hist)
-    procent = int(sum*0.1)
-    print (procent, ' ', len(hist))
+    s = np.sum(hist)
+    l = len(hist)
+    procent = int(s*percent)
+    print (procent, ' ', l)
+    minIdx = 0
+    maxIdx = l
     plus = 0
-    for i in range (0, len(hist)):
+    for i in range (0, l):
         plus += hist[i]
         if plus >= procent:
-            minimum = i - 1
-            print (minimum)
+            minIdx = i - 1
             break
         
     plus = 0
-    hist = hist[::-1]
-    for i in range (0, len(hist)):
-        plus += hist[i]
+    l = l - 1
+    for i in range (0, l):
+        plus += hist[l - i]
         if plus >= procent:
-            maximum = len(hist) - i
-            print (maximum)          
+            maxIdx = l - i
             break
     
-    hist = hist[::-1]  
+    step = (maxHist - minHist)/(bins-1)
+    
+    minimum = minHist + minIdx * step
+    maximum = minHist + (maxIdx + 1) * step
+    
+    print(minIdx, minimum)
+    print(maxIdx, maximum)
 
-#    for n in hist:
-#        sum_per += n
-#        sum_per = float(sum_per)
-#        hist_vos.append(sum_per)
-#
-#    plt.plot(hist_vos)
-#    plt.show()
     return minimum, maximum
 
     
-folder = r'D:\NOU2020\EarthExplorer\nnovgorod\2018\23-JUN\REFLECTANCE\Landsat_B'
+folder = r'E:\GIS\Fire_detection\result\Landsat_119041_20140807_20170420_B'
 b1 = np.load(folder + '1.npy')
 b2 = np.load(folder + '2.npy')
-#b3 = np.load(folder + '3.npy')
 b4 = np.load(folder + '4.npy')
 b5 = np.load(folder + '5.npy')
-#b6 = np.load(folder + '6.npy')
 b7 = np.load(folder + '7.npy')
-#b8 = np.load(folder + '8.npy')
-#b9 = np.load(folder + '9.npy')
-#b10 = np.load(folder + '10.npy')
-#b11 = np.load(folder + '11.npy')
 
+MNDWI   = getMNDWI(b2, b5)
+# NDWI    = getNDWI(b2, b4)
+# AWEInsh = getAWEInsh(b2, b4, b5, b7)
+# AWEIsh  = getAWEIsh(b1, b2, b4, b5, b7)
 
+b2 = None
+b4 = None
+b5 = None
+b6 = None
+gc.collect()
 
-#1 0.435–0.451 Coastal Aerosol (CA)
-#2 0.452–0.512 Blue
-#3 0.533–0.590 Green
-#4 0.636–0.673 Red
-#5 0.851–0.879 Near Infrared (NIR)
-#6 1.566–1.651 Shortwave NIR 1 (SWIR1)
-#7 2.107–2.294 Shortwave NIR 2 (SWIR2)
+bins = 100    
 
-""" MNDWI = (Blue − NIR)/(Blue + NIR) """
-a = b2 - b5
-b = b2 + b5
-b[b == 0] = 1
-MNDWI = np.divide(a,b)
-print (np.min(MNDWI),' ',np.max(MNDWI))
+minMNDWI = np.min(MNDWI)
+maxMNDWI = np.max(MNDWI)
+print (minMNDWI,' ',maxMNDWI)
 plt.title("MNDWI")
-h1 = histogram(MNDWI, 100, np.min(MNDWI), np.max(MNDWI))
+fMNDWI = MNDWI.ravel()
+print(fMNDWI.shape)
+h1 = np.histogram(fMNDWI, bins)
+print(h1[0].shape)
+# h1 = histogram(MNDWI, bins, minMNDWI, maxMNDWI)
+plt.plot(h1[0])
+plt.show()
+minimum, maximum = obrez(h1[0], minMNDWI, maxMNDWI, 0.01)
+print(minimum, maximum)
+h2 = np.histogram(fMNDWI, bins,( minimum, maximum))
+plt.title("MNDWI h2")
+plt.plot(h2[0])
+plt.show()
 
-mi_ma = obrez(h1)
-h1_v2 = hist_min_max(MNDWI, 100, mi_ma[0], mi_ma[1], np.min(MNDWI), np.max(MNDWI))
+minimum, maximum = obrez(h2[0], minimum, maximum, 0.01)
+h3 = np.histogram(fMNDWI, bins,( minimum, maximum))
+plt.title("MNDWI h3")
+plt.plot(h3[0])
+plt.show()
 
 
+minimum, maximum = obrez(h3[0], minimum, maximum, 0.01)
+h4 = np.histogram(fMNDWI, bins,( minimum, maximum))
+plt.title("MNDWI h4")
+plt.plot(h4[0])
+plt.show()
 
-# """ NDWI = (Blue − Red)/(Blue + Red) """
-# a = b2 - b4
-# b = b2 + b4
-# b[b == 0] = 1
-# NDWI = np.divide(a,b)
+minimum, maximum = obrez(h4[0], minimum, maximum, 0.01)
+h5 = np.histogram(fMNDWI, bins,( minimum, maximum))
+plt.title("MNDWI h5")
+plt.plot(h5[0])
+plt.show()
+
+minimum, maximum = obrez(h5[0], minimum, maximum, 0.01)
+h6 = np.histogram(fMNDWI, bins,( minimum, maximum))
+plt.title("MNDWI h6")
+plt.plot(h6[0])
+plt.show()
+
 # print (np.max(NDWI),' ',np.min(NDWI))
 # plt.title("NDWI")
 # h2 = histogram(NDWI, 100, np.min(NDWI), np.max(NDWI))
 # mi_ma = obrez(h2)
 
 
-
-# """ AWEInsh = 4*(Blue - Near) - (0.25*Red + 2.75*SWIR2) """
-# AWEInsh = 4*(b2 - b5) - (0.25*b4 + 2.75*b7)
 # print (np.max(AWEInsh),' ',np.min(AWEInsh))
 # plt.title("AWEInsh")
 # h3 = histogram(AWEInsh, 100, np.min(AWEInsh), np.max(AWEInsh))
 # mi_ma = obrez(h3)
 
 
-
-# """ AWEIsh = CA + 2.5*Blue - 1.5*(Red + Near) - 0.25*SWIR2 """
-# AWEIsh = b1 + 2.5*b2 - 1.5*(b4 + b5) - 0.25*b7
 # print (np.max(AWEIsh),' ',np.min(AWEIsh))
 # plt.title("AWEIsh")
 # h4 = histogram(AWEIsh, 100, np.min(AWEIsh), np.max(AWEIsh))
 # mi_ma = obrez(h4)
-
 
 
 
